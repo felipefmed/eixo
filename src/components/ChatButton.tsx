1c186c691c8af017574159ae52dbde65df65f7e3
Felipe Medeiros <48027498+felipefmed@users.noreply.github.com>
2025-05-25 16:39:52 -0300

    Update ChatButton.tsx

--- src/components/ChatButton.tsx
+++ src/components/ChatButton.tsx
@@ -66,7 +66,7 @@ const ChatButton = () => {
           onClick={() => setIsOpen(prev => !prev)}
           style={{ width: '64px', height: '64px' }} // Making the chat button bigger
         >
-          {isOpen ? <X size={32} /> : <MessageCircle size={32} />}
+          {isOpen ? <X size={48} /> : <MessageCircle size={48} />}
         </Button>
       </PopoverTrigger>
       <PopoverContent 
